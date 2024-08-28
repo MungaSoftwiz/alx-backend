a7f1b7c68fc0a448cc5fb00bcfa39098888f85b8
MungaSoftwiz <mwachilumobm@gmail.com>
2024-08-28 18:50:13 +0300

    Modify to make the displaySchoolValue async

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,34 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+import redis from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+const getAsync = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName) {
+  try {
+    const value = await getAsync(schoolName);
+    console.log(value);
+  } catch(err) {
+    console.error('Error fetching value:', err);
+  }
+}
+
+(async () => {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+})();
