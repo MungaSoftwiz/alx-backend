a7631596487631b6f53cc49642a0a82557b025a6
MungaSoftwiz <mwachilumobm@gmail.com>
2024-08-29 00:20:29 +0300

    Create a job and a queue for each job in array

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,29 @@
+function createPushNoticationsJobs (jobs, queue) {
+  if (!Array.isArray(jobs)) { throw new Error('Jobs is not an array'); }
+
+  jobs.forEach((element) => {
+    const job = queue.createJob('push_notification_code_3', element)
+
+    /* .save directly attached to save so works in test mode too */
+    job.save((err) => {
+      if (!err) {
+        console.log(`Notification job created: ${job.id}`);
+      }
+    });
+
+    job.on('complete', (result) => {
+      console.log(`Notification job ${job.id} completed`);
+    });
+
+    job.on('failed', (errMessage) => {
+      console.log(`Notification job ${job.id} failed: ${errMessage}`);
+    });
+
+    job.on('progress', (progress, data) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+  });
+}
+
+
+export default createPushNoticationsJobs;
