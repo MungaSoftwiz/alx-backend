872b8efb20c46cc75c32028aa7950765d5b01adc
MungaSoftwiz <mwachilumobm@gmail.com>
2024-09-05 13:47:57 +0300

    Get the hypermedia pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+""" Helper module to return paginated pages """
+import csv
+import math
+from typing import Any, Dict, List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Returns a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for the given
+    pagination parameters.
+
+    Parameters:
+    page (int): The page number (1-indexed).
+    page_size (int): The number of items per page.
+
+    Returns:
+    tuple: A tuple containing the start index and the end index.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Return paginated pages """
+        dataset = self.dataset()
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        indices = index_range(page, page_size)
+        data = []
+        if indices[-1] >= len(dataset):
+            return data
+        for idx in range(indices[0], indices[-1]):
+            data.append(self.__dataset[idx])
+        return data
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        """ Get the hypermedia pagination """
+        data = self.get_page(page, page_size)
+        total_pages = len(self.dataset()) / page_size
+        if len(self.dataset()) % page_size != 0:
+            total_pages += 1
+        return {
+            'page_size': page_size,
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if page + 1 <= total_pages else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': int(total_pages)
+        }
